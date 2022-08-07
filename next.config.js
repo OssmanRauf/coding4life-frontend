3d151dc44a8f7d861a467f11bce6d922e52ac797
ossman rauf <ossmanrauf8@gmail.com>
2022-08-07 17:28:35 +0200

    resolving deployment errors

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-import { info } from "./utils/tokens"
 const nextConfig = {
     reactStrictMode: true,
 }
 
-module.exports = {
-    images: {
-        domains: [info.baseUrl],
-    },
-    nextConfig,
-}
+module.exports = nextConfig
