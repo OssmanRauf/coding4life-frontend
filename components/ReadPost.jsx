bee7268092b9577a89828469df6b0a1a819d0b93
ossman rauf <ossmanrauf8@gmail.com>
2022-10-21 14:20:09 +0200

    adding created and updated time in post

--- components/ReadPost.jsx
+++ components/ReadPost.jsx
@@ -1,11 +1,24 @@
 import CommentSection from "./CommentSection"
 import styles from "../styles/readArticle.module.css"
+import { getDateInfo } from "../utils/tokens"
 const ReadPost = ({ post, comments }) => {
   return (
     <div>
       <div className={`${styles.article_container}`}>
         <div className={`${styles.article}`}>
           <h3 className={`${styles.article_title}`}>{post.title}</h3>
+          <p style={{ margin: 0, fontSize: "12px" }}>
+            Created:
+            <span style={{ marginLeft: 10 }}>
+              {getDateInfo(post.created_at)}
+            </span>
+          </p>
+          <p style={{ marginTop: 0, fontSize: "12px" }}>
+            Last updated:
+            <span style={{ marginLeft: 10 }}>
+              {getDateInfo(post.updated_at)}
+            </span>
+          </p>
           <div
             className={`${styles.article_content}`}
             dangerouslySetInnerHTML={{ __html: post.content }}
