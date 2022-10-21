d332769ad3ff05ce1b93a1a11148254cca7427d1
ossman rauf <ossmanrauf8@gmail.com>
2022-10-22 01:47:55 +0200

    adding meta data in homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,16 @@ export default function Home({ posts, pages }) {
     <>
       <div className="main-container">
         <Head>
-          <meta name="keywords" content="web-development, programming" />
+          <meta
+            name="keywords"
+            content="web-development, programming, coding, coding4life, software"
+          />
+          <meta
+            name="description"
+            content={`Coding4life a blog where you can learn and share your knowledge with others`}
+          />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="language" content="English" />
           <title> Home </title>
         </Head>
         <ListArticles
