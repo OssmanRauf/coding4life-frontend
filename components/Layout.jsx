b5a810179363ee8d8ec5825cb455bb3ff0147f0d
ossman rauf <ossmanrauf8@gmail.com>
2022-10-23 18:30:53 +0200

    adding loading on url change

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,7 +2,29 @@ import Nav from "./Nav"
 import Footer from "./Footer"
 import Script from "next/script"
 import Head from "next/head"
+import Image from "next/image"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+import loading from "../public/loading.gif"
 const Layout = ({ children, categories }) => {
+  const router = useRouter()
+  const [showLoading, setShowLoading] = useState(false)
+  // const
+  useEffect(() => {
+    const handleStart = (url) => setShowLoading(true)
+    const handleComplete = (url) => setShowLoading(false)
+    router.events.on("routeChangeStart", handleStart)
+    router.events.on("routeChangeComplete", handleComplete)
+    router.events.on("routeChangeError", handleComplete)
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart)
+      router.events.off("routeChangeComplete", handleComplete)
+      router.events.off("routeChangeError", handleComplete)
+      // setShowLoading(false)
+    }
+  })
+
   return (
     <>
       <Script
@@ -32,6 +54,22 @@ const Layout = ({ children, categories }) => {
 
         {/* </Script> */}
       </Head>
+      {showLoading ? (
+        <div className="router-loading">
+          <Image
+            // layout="fill"
+            height="100%"
+            width="100%"
+            // className="loading"
+            style={{ margin: "auto" }}
+            src={loading.src}
+            alt=""
+          />
+        </div>
+      ) : (
+        ""
+      )}
+
       <Nav categories={categories} />
       <main style={{ minHeight: "500px" }} className="main-layout">
         {children}
